e8af7f175c8828b8b5e5943430d2998bb6e10e61
Apurv Verma <dapurv5@gmail.com>
2013-05-15 05:57:38 +0530

    term sentiment complete

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,7 +1,45 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+def get_dict(sent_file):
+    scores = {}
+    for line in sent_file:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    return scores
+
+def hw(sent_file, tweet_file):
+    scores = get_dict(sent_file)
+    scores_all = {}
+    for line in tweet_file:
+        raw_tweet = json.loads(line)
+        tweet_text = raw_tweet["text"]
+        sentiment = get_sentiment(tweet_text, scores)
+        for word in tweet_text.split(" "):
+            if(word in scores.keys()):
+                scores_all[word] = scores[word]
+            else:
+                if(word in scores_all.keys()):
+                    old_score = scores_all[word]
+                    new_score = (old_score + sentiment)/2.0
+                    scores_all[word] = new_score
+                else:
+                    scores_all[word] = sentiment
+    display_scores(scores_all)
+
+
+def display_scores(dict):
+    for key in dict.keys():
+        print key, dict[key]
+
+
+def get_sentiment(sentence, scores):
+    numeric_score = 0.0
+    for word in sentence.split(" "):
+        if(word in scores.keys()):
+            numeric_score += scores[word]
+    return numeric_score
+
 
 def lines(fp):
     print str(len(fp.readlines()))
@@ -9,9 +47,8 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    hw(sent_file, tweet_file)
 
 if __name__ == '__main__':
     main()
+
